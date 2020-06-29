69025a2f2f7412b0fbb03d7065731308471e43b0
19Nar <narinemkn@gmail.com>
2020-06-29 19:42:29 +0200

    Updates

---
+++ precache-manifest.b174fa6c4c7e91f94aba99e10c4cb91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a595740b7bf759401b08227df8987c9b",
+    "url": "/Reactjs-MovieCMS/index.html"
+  },
+  {
+    "revision": "4f529a85ed76ec81fcfc",
+    "url": "/Reactjs-MovieCMS/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "5819a36a1e1fed980cd2",
+    "url": "/Reactjs-MovieCMS/static/css/main.d070ce72.chunk.css"
+  },
+  {
+    "revision": "4f529a85ed76ec81fcfc",
+    "url": "/Reactjs-MovieCMS/static/js/2.e4f7dbf2.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/Reactjs-MovieCMS/static/js/2.e4f7dbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5819a36a1e1fed980cd2",
+    "url": "/Reactjs-MovieCMS/static/js/main.eef54881.chunk.js"
+  },
+  {
+    "revision": "7975e96ec986de40d523",
+    "url": "/Reactjs-MovieCMS/static/js/runtime-main.03d9a264.js"
+  }
+]);

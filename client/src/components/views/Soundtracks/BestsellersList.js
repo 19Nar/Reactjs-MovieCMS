2cd0b483d42b99891cbed208b833d2474349e108
Narine Vanbeginne <60644251+19Nar@users.noreply.github.com>
2020-06-30 13:23:23 +0200

    Update BestsellersList.js

--- client/src/components/views/Soundtracks/BestsellersList.js
+++ client/src/components/views/Soundtracks/BestsellersList.js
@@ -9,9 +9,9 @@ function BestsellersList(props) {
         {props.sellers.map((album) => (
           <li key={album.collectionId}>
             <p>Name: {album.artistName}</p>
-            <p>See: <a href="">{album.artistViewUrl}</a></p>
+            <p>See: <a href={album.artistViewUrl}>{album.artistName}</a></p>
             <p>Collection's Name: {album.collectionName}</p>
-            <p>See: <a href="">{album.collectionViewUrl}</a></p>
+            <p>See: <a href={album.collectionViewUrl}>{album.collectionName}</a></p>
             <p>Released Date: {album.releaseDate}</p>
             <p>Country: {album.country}</p>
             <p>Collection Name: {album.collectionName}</p>
